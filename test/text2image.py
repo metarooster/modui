60bd9b73d10287272aa8d08e726fd94a6b67420a
Chai Chaoweeraprasit <wchao1115@hotmail.com>
2023-02-18 13:21:08 -0800

    Start integrating with modui.

--- test/text2image.py
+++ test/text2image.py
@@ -6,5 +6,4 @@ pipeline = StableDiffusionOnnxPipeline.from_pretrained("./stable_diffusion_onnx"
 def generate(prompt: str):
     return pipeline(prompt).images[0]
 
-generate("a photo of Elon Musk and Bill Gates fighting in a boxing match.").save("./sd_result.png")
-#modui.Page(generate, pipeline.scheduler.config).launch()
+modui.Page(generate, pipeline.scheduler.config).launch()
